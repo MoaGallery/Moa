fb8149f70130f8603465172544eaf1301c110965
danbrown <danbrown@6b71041f-f437-415a-8274-b101e94d25c2>
2009-07-23 20:57:31 +0000

    Sync to 1.2.0
    git-svn-id: svn://svn.code.sf.net/p/moagallery/code/Moagallery@26 6b71041f-f437-415a-8274-b101e94d25c2

---
+++ templates/MoaDefault/component_admin_user_container.php
@@ -0,0 +1,1 @@
+<moavar AdminUserList>
---
+++ templates/MoaDefault/style_inline.php
@@ -0,0 +1,18 @@
+.thumbsize
+{
+        width: <moatag type="ThumbWidth" add="20">px;
+        height: <moatag type="ThumbHeight" add="20">px;
+}
+
+.gal_thumbsize
+{
+        width: <moatag type="ThumbWidth">px;
+        height: <moatag type="ThumbHeight">px;
+}
+
+.thumbframe
+{
+        margin: 10px;
+        background-repeat: no-repeat;
+        background-position: center;
+}
